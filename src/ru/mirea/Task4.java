6c4159dc4036180e274752cac5ccb06a9de625b9
Sergey-hub02 <s.park190802@gmail.com>
2021-09-16 19:32:53 +0300

    Выполнил задание 4: Даны натуральные числа k и s. Определите, сколько существует k-значных натуральных чисел, сумма цифр которых равна d. Запись натурального числа не может начинаться с цифры 0.

--- src/ru/mirea/Task4.java
+++ src/ru/mirea/Task4.java
@@ -6,7 +6,38 @@ import java.util.*;
 public class Task4 {
   private static final Scanner IN = new Scanner(System.in);
 
+  /**
+   * Вычисляет сумму цифр числа
+   * @param number        число, сумму цифр которого нужно найти
+   * @return              сумма цифр целого числа
+   */
+  private static int sumOfDigits(int number) {
+    if (number < 10) {
+      return number;
+    }
+
+    return number % 10 + sumOfDigits(number / 10);
+  }
+
   public static void main(String[] args) {
-    ;
+    System.out.print("Введите k: ");
+    int k = IN.nextInt();
+
+    System.out.print("Введите s: ");
+    int s = IN.nextInt();
+
+    int start = (int) Math.pow(10, k - 1);
+    int end = (int) Math.pow(10, k);
+
+    int count = 0;
+
+    for (int num = start; num < end; ++num) {
+      if (sumOfDigits(num) == s) {
+        ++count;
+      }
+    }
+
+    System.out.println();
+    System.out.println("Количество " + k + "-значных чисел, сумма цифр которых равна " + s + ": " + count);
   }
 }
---
+++ src/ru/mirea/Task5.java
@@ -0,0 +1,4 @@
+package ru.mirea;
+
+public class Task5 {
+}
