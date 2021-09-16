8e5f499cdbe6242b7e373859702ed34e8b902a30
Sergey-hub02 <s.park190802@gmail.com>
2021-09-16 17:33:53 +0300

    Инициализирующий коммит

---
+++ src/ru/mirea/Task4.java
@@ -0,0 +1,12 @@
+package ru.mirea;
+
+import java.util.*;
+
+
+public class Task4 {
+  private static final Scanner IN = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    ;
+  }
+}
