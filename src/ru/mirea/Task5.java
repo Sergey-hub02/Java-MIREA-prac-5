02cd4559aabc734d36a0f3016e498f79be4c5fd4
Sergey-hub02 <s.park190802@gmail.com>
2021-09-16 19:43:01 +0300

    Выполнил задание 5: Дано натуральное число N. Найти сумму его цифр

--- src/ru/mirea/Task5.java
+++ src/ru/mirea/Task5.java
@@ -1,4 +1,22 @@
 package ru.mirea;
 
+import java.util.*;
+
 public class Task5 {
+  private static final Scanner IN = new Scanner(System.in);
+
+  private static int sumOfDigits(int number) {
+    if (number < 10) {
+      return number;
+    }
+
+    return number % 10 + sumOfDigits(number / 10);
+  }
+
+  public static void main(String[] args) {
+    System.out.print("Введите число: ");
+    int n = IN.nextInt();
+
+    System.out.println("Сумма цифр числа " + n + ": " + sumOfDigits(n));
+  }
 }
